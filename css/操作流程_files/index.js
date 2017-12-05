3436acd5000d6ddb7395b8ab15baaa230b9921d6
airbreak <jiangjianming_m@163.com>
2017-12-06 00:52:46 +0800

    finish fn except bg-color

---
+++ css/操作流程_files/index.js
@@ -0,0 +1,43 @@
+$(function(){
+	var curIndex=0;
+	var timeInterval=500;
+	var arr=new Array();
+	arr[0]="img/lineOne.png";
+	arr[1]="img/lineTwo.png";
+	setInterval(changeImg,timeInterval);
+	function changeImg(){
+ 		if(curIndex==arr.length-1){
+ 			curIndex=0;	
+ 		}else{
+ 			curIndex+=1;
+ 		}
+ 		$('.bg').css("background-image",'url('+arr[curIndex]+')');
+ 	}
+
+ 	$('button').click(function(){
+ 		_hmt.push(['_trackEvent', "procedure-dist", "procedure-distClick"]);
+ 		$(location).attr('href','slider.html')
+ 	})
+ 	$('.dowload').click(function(){
+ 			var u = navigator.userAgent;
+            var isAndroid = u.indexOf('Android ') > -1|| u.indexOf('Linux') > -1; //android终端或者uc浏览器
+            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+            if( isAndroid  ){
+            	_hmt.push(['_trackEvent', "procedure-dist", "20171124-01Q00PdownloadAppAndroid"]);
+            	 window.location.href = " http://zhcn.web.cdn.cootekservice.com/Downloads/ChuBaoDianHua_01Q00D.apk";
+            }
+            if( isiOS  ){
+            	_hmt.push(['_trackEvent', "procedure-dist", "20171124-01Q00PClickDownloadAppIOS"]);
+            	window.location.href = "http://dialer.cdn.cootekservice.com/web/external/laststep/index.html?code=01Q00P";
+            }
+
+        })
+ 	
+
+
+
+
+
+ })
+
+
